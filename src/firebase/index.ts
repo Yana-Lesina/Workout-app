c85eee5376a34e1fedc8f23fd042eb3c701ad600
Yana-Lesina <yanalesina3@gmail.com>
2022-07-29 21:13:20 +0300

    error: wrapp file for solving firebase apikey error

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,17 +2,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import {
+  apiKey,
+  authDomain,
+  projID,
+  storageBucket,
+  senderID,
+  appID,
+} from "../forEnv";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+export const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projID,
+  storageBucket: storageBucket,
+  messagingSenderId: senderID,
+  appId: appID,
 };
 
 // Initialize Firebase
@@ -20,5 +29,3 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-export default app;
