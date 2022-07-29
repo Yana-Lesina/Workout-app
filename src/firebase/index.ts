45acbeb776c3539b9ae106495b699011fd55845c
Yana-Lesina <yanalesina3@gmail.com>
2022-07-29 23:12:45 +0300

    error: change firebase funct imports hope it'll help

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 import {
   fApiKey,
@@ -15,17 +18,20 @@ import {
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const app = firebase.initializeApp({
   apiKey: fApiKey,
   authDomain: fAuthDomain,
   projectId: fProjID,
   storageBucket: fStorageBucket,
   messagingSenderId: fSenderID,
   appId: fAppID,
-};
+});
+
+const auth = app.auth();
+export default auth;
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+// export const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
