d8120c566d1eb5da92365339bb2d6a93495876f4
Yana-Lesina <yanalesina3@gmail.com>
2022-08-16 22:54:07 +0300

    fix: firebase & netlify problem

--- src/firebase/databaseFuncs.ts
+++ src/firebase/databaseFuncs.ts
@@ -1,17 +1,18 @@
 import { getDatabase, ref, child, get } from "firebase/database";
-import { WorkoutPartType } from "../globalTypes";
+import { DataType, WorkoutPartType } from "../globalTypes";
 import { setOverviewPageLoaded } from "../redux-store/slices/loadersSlice";
 import { setWorkoutsList } from "../redux-store/slices/workoutsArraySlice";
 import { store } from "../redux-store/store";
 
 export const getServerData = async () => {
   const dbRef = ref(getDatabase());
-  let serverData: WorkoutPartType[] = [] as WorkoutPartType[];
-  get(child(dbRef, `/data/`))
+  let serverData: DataType; //WorkoutPartType[] = [] as WorkoutPartType[];
+  get(child(dbRef, `/`))
     .then(async (snapshot) => {
       if (snapshot.exists()) {
         serverData = await snapshot.val();
-        store.dispatch(setWorkoutsList(serverData));
+        let workouts = serverData.data;
+        store.dispatch(setWorkoutsList(workouts));
         store.dispatch(setOverviewPageLoaded(true));
         // return serverData;
       } else {
