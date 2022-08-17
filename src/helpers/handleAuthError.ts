948207ccbbf13db92f5efceb808ffa37e553a6a3
Yana-Lesina <yanalesina3@gmail.com>
2022-08-17 11:35:54 +0300

    dev: return .toml to norm state

--- src/helpers/handleAuthError.ts
+++ src/helpers/handleAuthError.ts
@@ -23,25 +23,3 @@ export const handleAuthError = (error: Error) => {
     return error.message;
   }
 };
-
-// export const handleAuthError = (error: Error) => {
-//   console.log("here we go handleError block hello");
-
-//   if (error instanceof FirebaseError) {
-//     const devErrorCode = error.code;
-
-//     if (devErrorCode === "auth/invalid-email") {
-//       store.dispatch(setError("Incorrect email syntax"));
-//     } else if (devErrorCode === "auth/weak-password") {
-//       store.dispatch(setError("Password should be at least 6 characters "));
-//     } else if (devErrorCode === "auth/email-already-in-use") {
-//       store.dispatch(setError("There's already an account for this email"));
-//     } else if (devErrorCode === "auth/wrong-password") {
-//       store.dispatch(setError("Incorrect password"));
-//     } else if (devErrorCode === "auth/user-not-found") {
-//       store.dispatch(setError("There's no such user"));
-//     } else store.dispatch(setError(error.message));
-//   } else {
-//     store.dispatch(setError(error.message));
-//   }
-// };
